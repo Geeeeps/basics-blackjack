03530680137d909be7e9da3bf6cd6601c2492f93
Geeeeps <tgeeping@gmail.com>
2022-03-01 20:14:13 +0800

    blackjack file commit

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// Create a deck of cards
+// Shuffle the deck
+// Deal two cards to player and computer
+// User determines whether to hit or stand
+// If hit, deal player with another card
+// If stand & if computer cards < 17, hit computer another card, else computer stands
+// If sum of player cards > sum of com cards, player wins
+// If sum of com cards > sum of player cards, com wins
+
+var shuffleDeck = function (deck) {};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
